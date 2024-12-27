d3b5a9bb6f86c5f90248748b126bf5604ba3b8ad
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2024-12-27 22:06:49 +0200

    creates theme

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+"use client";
+
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "hsl(280, 100%, 70%)",
+    },
+    text: { primary: "#fff" },
+  },
+});
