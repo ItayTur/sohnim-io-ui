11d1746097fb6bb4dc649574f2313bb25fc9cc67
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2024-12-28 14:43:23 +0200

    creates `leadRouter`

--- src/app/_components/Leads/LeadsTable/LeadsTable.tsx
+++ src/app/_components/Leads/LeadsTable/LeadsTable.tsx
@@ -1,7 +1,8 @@
+import { api } from "@/trpc/server";
 import { Table } from "../../UI";
-import { leads } from "./LeadsTable.consts";
 import { columns } from "./LeadTabls.columns";
 
-export const LeadsTable = () => {
-  return <Table rows={leads} columns={columns} />;
+export const LeadsTable = async () => {
+  const leads = await api.lead.getLeads();
+  return <Table rows={leads} loading={!leads} columns={columns} />;
 };
