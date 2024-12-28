a599d486e0008e7badcad54057615c54dc2600b3
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2024-12-28 15:30:02 +0200

    Creates `Dialog` component

---
+++ src/app/_components/UI/Dialog/Dialog.tsx
@@ -0,0 +1,5 @@
+import { Dialog as MuiDialog, type DialogProps } from "@mui/material";
+
+export const Dialog = (props: DialogProps) => {
+  return <MuiDialog {...props} />;
+};
--- src/app/_components/UI/index.tsx
+++ src/app/_components/UI/index.tsx
@@ -1,3 +1,4 @@
 export * from "./Button/Button";
+export * from "./Dialog/Dialog";
 export * from "./Table/Table";
 export * from "./Text/Text";
