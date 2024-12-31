f7d7b2a004317cf5f4ff94f9da78dc2d71857018
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2024-12-31 19:17:36 +0200

    removes duplicated productColumns

--- src/app/_components/Products/ProductCategoriesTable/ProductCategoriesTable.columns.tsx
+++ src/app/_components/Products/ProductCategoriesTable/ProductCategoriesTable.columns.tsx
@@ -1,28 +1,6 @@
-import { type GridColDef, type GridRenderCellParams } from "@mui/x-data-grid";
-import { type Product } from "../Products.types";
-import { ProductsTableMenu } from "./ProductsTableMenu";
-
-type ProductsGridRenderCellParams = GridRenderCellParams<Product>;
+import { type GridColDef } from "@mui/x-data-grid";
 
 export const productCategoryColumns: GridColDef[] = [
   { field: "id", headerName: "Category ID", width: 100 },
   { field: "name", headerName: "Category Name", width: 200 },
-];
-
-export const productColumns: GridColDef[] = [
-  { field: "id", headerName: "Product ID", width: 70 },
-  { field: "name", headerName: "Product Name", width: 130 },
-  { field: "price", headerName: "Price", type: "number", width: 90 },
-  { field: "description", headerName: "Description", width: 200 },
-  {
-    headerName: "",
-    type: "actions",
-    field: "actions",
-    align: "right",
-    renderCell: ({ row }: ProductsGridRenderCellParams) => (
-      <ProductsTableMenu />
-    ),
-    sortable: false,
-    filterable: false,
-  },
 ];
